a812dd9a7c27602caec9cdd46c06e82a9a83f60e
krgkaushik <kaushikgunjkar52@gmail.com>
2026-04-05 18:41:51 +0530

    Add_new_pattern

---
+++ Pattern/Square_star_pattern.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class solution {
+public:
+    void printSquarePattern(int n) {
+        // Write your code here...
+        for(int i =0; i<n;i++){
+            for(int j=0 ;j<n;j++){
+                cout<<"*"<<" ";
+                
+            }
+            cout<<endl;
+        }
+        
+        
+    }
+};
